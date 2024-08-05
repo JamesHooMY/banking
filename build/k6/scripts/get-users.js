0d4e0f6da7a8bd6ca78196b5ba1e4911a0add0ca
JamesHooMY <hoosinyong@hotmail.com>
2024-08-05 22:22:40 +0800

    feat: update grafana docker images for supporting arm64

--- build/k6/scripts/get-users.js
+++ build/k6/scripts/get-users.js
@@ -3,9 +3,9 @@ import { check, sleep } from 'k6';
 
 export let options = {
     stages: [
-        { duration: '30s', target: 20 },
-        { duration: '1m', target: 20 },
-        { duration: '10s', target: 0 },
+        { duration: '30s', target: 20 }, // increase users from 0 to 20 over 30s
+        { duration: '1m', target: 20 }, // stay at 20 users for 1m
+        { duration: '10s', target: 0 }, // ramp down to 0 users over 10s
     ],
 };
 
